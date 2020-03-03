12229af1ec3ea9800130fd2c46e707403eb12d91
yudhiesh1997 <yudhiesh1997@gmail.com>
2020-03-03 21:25:45 +0800

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Yudhiesh Ravindranath on 03/03/2020.
+//  Copyright © 2020 Yudhiesh Ravindranath. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
